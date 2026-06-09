9a50e69865b217b2110198cc5519b47dd13a8b97
acheng1757 <abbiec1757@gmail.com>
2026-06-09 15:36:39 -0700

    .

---
+++ macro/intuition_test/2_ethanol_ccs_lf/user_additions/usercommodities.jl
@@ -0,0 +1,14 @@
+abstract type Fossil_NaturalGas <: MacroEnergy.NaturalGas end
+abstract type Biomass_Wood <: MacroEnergy.Biomass end
+abstract type Biomass_Herb <: MacroEnergy.Biomass end
+abstract type Biomass_Agri <: MacroEnergy.Biomass end
+abstract type Gasoline <: MacroEnergy.LiquidFuels end
+abstract type JetFuel <: MacroEnergy.LiquidFuels end
+abstract type Diesel <: MacroEnergy.LiquidFuels end
+abstract type Fossil_Gasoline <: MacroEnergy.LiquidFuels end
+abstract type Fossil_JetFuel <: MacroEnergy.LiquidFuels end
+abstract type Fossil_Diesel <: MacroEnergy.LiquidFuels end
+abstract type Ethanol <: MacroEnergy.LiquidFuels end
+abstract type Upstream_Ethanol <: MacroEnergy.LiquidFuels end
+abstract type Fossil_Ethylene <: MacroEnergy.Ethylene end
+abstract type Biomass_Corn <: MacroEnergy.Biomass end
