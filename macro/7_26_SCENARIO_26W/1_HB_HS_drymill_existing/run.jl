504083a6ea17fcbf79a4a7d3d0612a7ce0227238
acheng1757 <abbiec1757@gmail.com>
2026-07-27 03:20:05 -0700

    .

--- macro/7_26_SCENARIO_26W/1_HB_HS_drymill_existing/run.jl
+++ macro/7_26_SCENARIO_26W/1_HB_HS_drymill_existing/run.jl
@@ -3,5 +3,5 @@ using Gurobi
 
 (case, solution) = run_case(@__DIR__; 
     optimizer=Gurobi.Optimizer,
-    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3)
+    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3, "BarHomogeneous" => 1)
 );
