042e983a16bd0b81987297e8329e4184d6edc721
acheng1757 <abbiec1757@gmail.com>
2026-07-27 18:28:56 -0700

    .

--- macro/7_27_SCENARIO_11w/1_HB_HS_drymill_existing/run.jl
+++ macro/7_27_SCENARIO_11w/1_HB_HS_drymill_existing/run.jl
@@ -1,7 +1,10 @@
 using MacroEnergy
-using Gurobi
+using Gurobi # or HiGHS, CPLEX, etc.
 
-(case, solution) = run_case(@__DIR__; 
-    optimizer=Gurobi.Optimizer,
-    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3, "BarHomogeneous" => 1)
+(case, solution) = run_case(
+    @__DIR__;
+    planning_optimizer=Gurobi.Optimizer, # Optimizer Constructor for the planning problem
+    subproblem_optimizer=Gurobi.Optimizer, # Optimizer Constructor for the subproblems
+    planning_optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3, "BarHomogeneous" => 1), # Optimizer Settings for the planning problem
+    subproblem_optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3, "BarHomogeneous" => 1), # Optimizer Settings for the subproblems
 );
